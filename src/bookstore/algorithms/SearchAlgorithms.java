69961124136682660f79c8f19dba3d3eb81486c0
minZ934 <minhnhgcd220471@fpt.edu.vn>
2025-04-11 20:46:33 +0700

    Update SearchAlgorithms.java

--- src/bookstore/algorithms/SearchAlgorithms.java
+++ src/bookstore/algorithms/SearchAlgorithms.java
@@ -5,17 +5,20 @@ import bookstore.models.Order;
 
 public class SearchAlgorithms {
     public static Order linearSearch(CustomQueue<Order> queue, int orderId) {
-        CustomQueue<Order> tempQueue = new CustomQueue<>(queue.size());
+        CustomQueue<Order> tempQueue = new CustomQueue<>(); 
+
         Order foundOrder = null;
 
+        // Search through the queue
         while (!queue.isEmpty()) {
             Order order = queue.dequeue();
             if (order.getOrderId() == orderId) {
                 foundOrder = order;
             }
-            tempQueue.enqueue(order);
+            tempQueue.enqueue(order); // Save for restoring
         }
 
+        // Restore the original queue
         while (!tempQueue.isEmpty()) {
             queue.enqueue(tempQueue.dequeue());
         }
