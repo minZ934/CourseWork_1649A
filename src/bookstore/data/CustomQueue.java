35152f6010e279647ed399f5e70914dc51ec77ef
minZ934 <minhnhgcd220471@fpt.edu.vn>
2025-04-11 20:44:37 +0700

    Update CustomQueue.java

--- src/bookstore/data/CustomQueue.java
+++ src/bookstore/data/CustomQueue.java
@@ -1,38 +1,70 @@
 package bookstore.data;
 
 public class CustomQueue<T> {
-    private T[] queue;
-    private int front, rear, size, capacity;
+    private static class Node<T> {
+        T data;
+        Node<T> next;
 
-    @SuppressWarnings("unchecked")
-    public CustomQueue(int capacity) {
-        this.capacity = capacity;
-        queue = (T[]) new Object[capacity];
-        front = 0;
-        rear = -1;
-        size = 0;
-    }
-
-    public void enqueue(T item) {
-        if (size == capacity) {
-            System.out.println("Queue is full!");
-            return;
+        Node(T data) {
+            this.data = data;
         }
-        rear = (rear + 1) % capacity;
-        queue[rear] = item;
-        size++;
     }
 
-    public T dequeue() {
-        if (isEmpty()) return null;
-        T item = queue[front];
-        front = (front + 1) % capacity;
-        size--;
-        return item;
+    private Node<T> front;
+    private Node<T> rear;
+    private int size;
+
+    public CustomQueue() {
+        front = rear = null;
+        size = 0;
     }
 
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    public void enqueue(T item) {
+        Node<T> newNode = new Node<>(item);
+        if (isEmpty()) {
+            front = rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+        size++;
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty!");
+            return null;
+        }
+        T item = front.data;
+        front = front.next;
+        if (front == null) rear = null;
+        size--;
+        return item;
+    }
+
+    public T peek() {
+        if (isEmpty()) return null;
+        return front.data;
+    }
+
+    public boolean contains(T item) {
+        Node<T> current = front;
+        while (current != null) {
+            if (current.data.equals(item)) return true;
+            current = current.next;
+        }
+        return false;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Index out of bounds");
+        Node<T> current = front;
+        for (int i = 0; i < index; i++) current = current.next;
+        return current.data;
     }
 
     public int size() {
